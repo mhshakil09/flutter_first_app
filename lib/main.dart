2eaa21849db25c58f06737237ea7fd6dd78648eb
Maruf <mhshakil09@gmail.com>
2021-11-23 16:41:57 +0600

    Workable till now. Dynamic number of button s

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_first_app/answer.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import './question.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  void answerQuestion() {
-    print("answer question");
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex += 1;
+    });
+
+    print("answer question");
+    Fluttertoast.showToast(
+        msg: "Question ${_questionIndex} answered",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0);
   }
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      {
+        'questionText': "what's your favorite color?",
+        'answers': ['black', 'blue', 'green', 'red']
+      },
+      {
+        'questionText': "what's your favorite food?",
+        'answers': ['sweets', 'spicy', 'ice-cream', 'all']
+      },
+      {
+        'questionText': "what's your favorite game?",
+        'answers': ['cs', 'bs', 'pd', 'vr']
+      },
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -17,19 +54,12 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text("The Question!"),
-            ElevatedButton(
-              child: Text("Answer 1"),
-              onPressed: answerQuestion,
+            // Text(questions[_questionIndex]),
+            Question(
+              questions[_questionIndex]['questionText'].toString(),
             ),
-            ElevatedButton(
-              child: Text("Answer 2"),
-              onPressed: answerQuestion,
-            ),
-            ElevatedButton(
-              child: Text("Answer 3"),
-              onPressed: answerQuestion,
-            ),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) => Answer(_answerQuestion ,answer)).toList()
           ],
         ),
       ),
